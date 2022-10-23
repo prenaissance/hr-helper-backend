eb789a4b26e43150a91115b64f97642e40a54ccd
milan <dprodan289@gmail.com>
2022-10-23 21:41:32 +0300

    convert by person

--- data-analysis/main.py
+++ data-analysis/main.py
@@ -1,24 +1,25 @@
 from lib import query
 from lib import data
 from sys import getsizeof, exit
+import matplotlib.pyplot as plt
 
 # if you haven't formatted data yet if you don't provide any arguments it will take the path as Input_Records-rjopma1bz5n6bxxvxhq0.json
 # data = data.Data()
 # data.savaData('formatted.json')
 
 # if you have formatted data do this cause it's faster
-data = data.Data(formatted_path='formatted.json')
+data = data.Data(formatted_path='formated.json')
 # make queries
 q = query.QueryJson(data.getData())
-print(q.getTotalConversions())
-print(q.getConversionBy('device'))
-print(q.getConversionBy('locale'))
-# print(q.getConversionByHour("+00:00"))
 
-# in order to sort by time do this
-time = sorted(q.getConversionByHour("-10:00").items(), key=lambda x: int(x[0]))
-print(time)
-print(q.getAllTypes('event'))
-print(q.getTotalBy({"event": ["conversion"],
-                    "locale": ["en-US"],
-                    }))
+keys_convert = q.getConvertKeys().keys()
+time_convert = []
+for key in keys_convert:
+    time_convert.append(q.getConvertTimeById(key))
+print(len(time_convert))
+print(len(keys_convert))
+for t in time_convert:
+    if t is None:
+        print("yes")
+plt.bar([i for i in range(len(keys_convert))], time_convert)
+plt.show()
